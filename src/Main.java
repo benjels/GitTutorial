6592551b4b7eb5becddd069bde41eca829c1d3b3
Max Brown <maxbrown0101@gmail.com>
2015-09-27 00:31:23 +1200

    added an extra println

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 
 public class Main {
 public static void main(String[] args){
-	//System.out.println("eat shit");
-	Printer.printIt("eat shit");
+	System.out.println("eat shit");
+	Printer.printIt("eat shit in printer");
+
 }
 }
