9391cdf6de812caa066cd10372b2414e05b99316
Mac Copley <qcopley@gmail.com>
2015-09-27 03:09:57 +1300

    added a new kind of printer

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public static void main(String[] args){
 	System.out.println("we back");
 	System.out.println("eat it all");
 	Printer2.print("nooo");
+	Printer3.printMaxC("string that maxc added");
 
 }
 }
---
+++ src/Printer3.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author copleyquen
+ *class i added for more printing options
+ */
+public class Printer3 {
+
+
+	public static void printMaxC(String string){
+		System.out.println(string + "max c is rly cool tbh");
+	}
+
+
+
+}
