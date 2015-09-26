d70b3bb843ee959f2d2623646742ce587acf3d79
Max Brown <maxbrown0101@gmail.com>
2015-09-27 01:41:06 +1200

    removed the printer class

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@
 public class Main {
 public static void main(String[] args){
 	System.out.println("eat shit");
-	Printer.printIt("eat shit in printer");
 	System.out.println("we back");
 	System.out.println("eat it all");
 
