65df51ca6e9a7c07eca1bd4fcaef63550f9e05aa
Max Brown <maxbrown0101@gmail.com>
2015-09-27 00:57:25 +1200

    added the bin files and added a print to main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public static void main(String[] args){
 	System.out.println("eat shit");
 	Printer.printIt("eat shit in printer");
 	System.out.println("we back");
+	System.out.println("eat it all");
 
 }
 }
